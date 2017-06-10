5a1d694cb81c4512f8e4cb3b310af7c95fc4f65c
weiweijun2006 <weiweijun2006@sina.com>
2017-06-10 16:31:53 +0800

    Create taobao.py

--- taobao.py
+++ taobao.py
@@ -4,26 +4,48 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pyquery import PyQuery as pq
-from config import *
-import pymongo
-MONGO_URL = 'localhost'
-MONGO_DB = 'taobao'
-MONGO_TABLE = 'product'
 
+import datetime  
+import time
 SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
-
-NAME = 'xxx'
-PSWD = 'xxx'
-client = pymongo.MongoClient(MONGO_URL)
-db = client[MONGO_DB]
-
+KEYWORD = '伟伟君'
+KEYWORD2 = 'wijnjg52'
 browser = webdriver.Chrome(service_args=SERVICE_ARGS)#PhantomJS
 wait = WebDriverWait(browser, 10)
 
 browser.set_window_size(1400, 900)
+#buytime = '2016-12-27 22:31:00' 
+def buy_on_time(buytime):
+    now = datetime.datetime.now()
+    print("准备抢票")
+    print("现在时间：",now.strftime('%Y-%m-%d %H:%M:%S'),"\r抢票时间:",buytime)
+    while True:
+        now = datetime.datetime.now()
 
+        if (now.strftime('%Y-%m-%d %H:%M:%S') == buytime):
+            print("时间到")
+            browser.refresh()
+            submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_SelectAll1')))
+            submit.click()
+            submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_SmallSubmit')))
+            submit.click()
+        #J-security > div.ui-securitycore.ui-securitycore-tip.J-securitycoreTip > div > div.ui-form-explain
+            submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#submitOrder_1 > div.wrapper > a.go-btn')))
+            submit.click()
+            
+            
+            #input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#J-security > div.ui-securitycore.ui-securitycore-tip.J-securitycoreTip > div > div.J-checkResult.fn-hide')))
+            input = wait.until(EC.presence_of_element_located((By.ID, 'payPassword_rsainput')))
+            input.send_keys('198726')#密码输入
 
+            submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_authSubmit')))
+            submit.click() 
+            
+            time.sleep(3)
+            print (now.strftime('%Y-%m-%d %H:%M:%S'))
+            print ('purchase success')
+            return
+        time.sleep(0.5)
 
 
 def logintaobao():
@@ -42,8 +64,8 @@ def logintaobao():
         input2 = wait.until( EC.presence_of_element_located((By.CSS_SELECTOR, '#TPL_password_1')))
         submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_SubmitStatic')))
         submit.click()#J_SelectAll1
-        input.send_keys(NAME)#J_SelectAll1 > div > label
-        input2.send_keys(PSWD)
+        input.send_keys(KEYWORD)#J_SelectAll1 > div > label
+        input2.send_keys(KEYWORD2)
         submit.click()
         submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_MtSideMenu > div > dl:nth-child(1) > dd:nth-child(2) > a')))
         submit.click()
@@ -54,22 +76,11 @@ def logintaobao():
             if handle!=search:
              browser.switch_to_window(handle)
         print(browser.page_source)
-        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_SelectAll1')))
-        submit.click()
-        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_SmallSubmit')))
-        submit.click()
-        #J-security > div.ui-securitycore.ui-securitycore-tip.J-securitycoreTip > div > div.ui-form-explain
-        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#submitOrder_1 > div.wrapper > a.go-btn')))
-        submit.click()
+
 
         #payPassword_container > div > i:nth-child(1)
 
-        input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#J-security > div.ui-securitycore.ui-securitycore-tip.J-securitycoreTip > div > div.J-checkResult.fn-hide')))
-        input = wait.until(EC.presence_of_element_located((By.ID, 'payPassword_rsainput')))
-        input.send_keys('xxxxxx')#密码输入
 
-        submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#J_authSubmit')))
-        submit.click()
         return
 
         #payPassword_container > div
@@ -81,3 +92,5 @@ def logintaobao():
 
 if __name__ == '__main__':
     logintaobao()
+    buytime = '2017-06-10 16:26:00' 
+    buy_on_time(buytime)
